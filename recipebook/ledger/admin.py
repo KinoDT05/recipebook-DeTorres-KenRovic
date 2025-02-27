01aefb953873007935a2d5e947481c8e37e633df
Kino De Torres <ken.detorres@student.ateneo.edu>
2025-02-27 23:35:17 +0800

    Added Initial Admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Ingredients, Recipe, RecipeIngredient
 # Register your models here.
+
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+
+
+# registering the model and the admin is what tells
+# Django that admin pages must be generated for the models specified
+admin.site.register(Recipe, RecipeAdmin)
