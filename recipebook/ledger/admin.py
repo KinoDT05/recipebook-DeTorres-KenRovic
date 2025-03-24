e8cb390e6155861359e69939e471d2a49cac53e9
Kino De Torres <ken.detorres@student.ateneo.edu>
2025-03-24 20:00:30 +0800

    part 1

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Ingredients, Recipe, RecipeIngredient
+from .models import Ingredients, Recipe, RecipeIngredient, RecipeImage
 # Register your models here.
 
 
 class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
+
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    inlines = [RecipeIngredientInLine,]
+    inlines = [
+        RecipeIngredientInLine,
+        RecipeImageInline,
+        ]
 
 admin.site.register(Recipe, RecipeAdmin)
--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -112,10 +112,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+    ]
+STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/recipe/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
+MEDIE_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
